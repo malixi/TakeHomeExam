af9fb4cefa1f2b2cf8e82fa25bee9cb3bb048cf4
Carlo <malixi@users.noreply.github.com>
2018-07-30 10:55:13 +0800

    working on responsiveness & media queries

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ $(window).on('scroll', function(){
     $('nav').removeClass('white');
   }
 });
+
+$(window).on('scroll', function(){
+
+  $('.fadetoleft').each(function(){
+    let pos = $(this).offset().top,
+        height = $(this).height(),
+        topOfWindow = $(window).scrollTop();
+
+    if(pos < topOfWindow + height && pos + height > topOfWindow){
+      $(this).addClass('fadeInLeft_animation');
+    }
+  });
+
+});
