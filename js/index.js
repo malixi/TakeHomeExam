01869211c0d778fa1360bd3c884e996835732bab
Carlo <malixi@users.noreply.github.com>
2018-07-31 10:00:47 +0800

    adding animation

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ $(window).on('scroll', function(){
   }
 });
 
+{
 var images=new Array('./images/slideshow1.jpg','./images/slideshow2.jpg','./images/slideshow3.jpg','./images/slideshow4.jpg','./images/slideshow5.jpg');
 var nextimage=0;
 slideshow();
@@ -21,16 +22,49 @@ function slideshow(){
 
 
 }
+}
 
-function slideshow(){
+{
+var images=new Array('./images/slideshow1.jpg','./images/slideshow2.jpg','./images/slideshow3.jpg','./images/slideshow4.jpg','./images/slideshow5.jpg');
+var nextimage=0;
+bgslideshow();
+
+function bgslideshow(){
     if(nextimage>=images.length){nextimage=0;}
     $('.main-page')
     .css('background-image','url("'+images[nextimage++]+'")')
     .fadeIn(3000,function(){
-        setTimeout(slideshow,6000);
+        setTimeout(bgslideshow,6000);
     });
 
 
-
-
 }
+}
+
+
+$(window).on('scroll', function(){
+
+  $('.leftToRight').each(function(){
+    let pos = $(this).offset().top,
+        height = $(this).height(),
+        topOfWindow = $(window).scrollTop();
+
+    if(pos < topOfWindow + height && pos + height > topOfWindow){
+      $(this).addClass('fadeToLeft');
+    }
+  });
+
+});
+
+
+$(window).on('scroll', function(){
+  $('.rightToLeft').each(function(){
+    let pos = $(this).offset().top,
+        height = $(this).height(),
+        topOfWindow = $(window).scrollTop();
+
+    if(pos < topOfWindow + height && pos + height > topOfWindow){
+      $(this).addClass('fadeInRight_animation');
+    }
+  });
+});
